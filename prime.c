5ea8422f331f7af9ba21b48fe925ee597cf9fb5c
sankarcse <sankarcse05@gmail.com>
2017-02-19 03:26:08 -0800

    Update prime.c

--- prime.c
+++ prime.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 int main()
 {
-	int n,count=0;
+	int n,count=0;//declaration
 	scanf("%d",&n);
 	for(int i=2;i<n;i++)
 	{
 		if(n%i==0)
 		{
-			coutn++;
+			coutn++;//counting
 		}
 	}
 	if(count==2)
 	printf("prime");
 	else
 	printf("not prime");
-}
+}//sankar
