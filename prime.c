28fff5aa33b412fe215f9e201a13002218277aae
sankarcse <sankarcse05@gmail.com>
2017-02-19 16:11:48 -0800

    initial commit

--- prime.c
+++ prime.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
 int main()
 {
-	int n;
+	int n,count=0;
 	scanf("%d",&n);
 	for(int i=2;i<n;i++)
 	{
-		
+		if(n%i==0)
+		{
+			coutn++;
 		}
+	}
+	if(count==2)
+	printf("prime");
+	else
+	printf("not prime");
 }
